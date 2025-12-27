47cd7c43d6d3770da3f6dafc780f8daa6bf606bc
Mayur-U <mayuru02@gmail.com>
2025-12-27 15:51:06 +0000

    onyx:add vendorsetup.sh
    Change-Id: I8032d7c2c092b24b6e01af4644cf4e628387db43

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Mayur-U/android_hardware_xiaomi_onyx.git hardware/xiaomi
+
+git clone https://github.com/Mayur-U/android_packages_apps_XiaomiDolby.git packages/apps/XiaomiDolby
+
+git clone https://github.com/Mayur-U/android_device_xiaomi_onyx-kernel.git device/xiaomi/onyx-kernel
+
+git clone https://gitlab.com/xiaomi-sm8750-onyx/proprietary_vendor_xiaomi_onyx.git -b sixteen-qpr1 vendor/xiaomi/onyx
